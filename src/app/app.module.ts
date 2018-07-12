87c1605077bb5e9d4f482a3c706852159a533226
Akash Thengre <AThengre@vidushigoc.local>
2018-07-12 22:00:08 +0530

    product result addition

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import { RoutingModule } from './routing/routing.module';
     HomeComponent
   ],
   imports: [
-    RoutingModule
+    RoutingModule,BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,6 +9,43 @@ export class HomeComponent implements OnInit {
 
   constructor() { }
 
+  items = [ {
+    id:"item1",
+    price:20,
+    qty:0,
+    text:"Add To Cart"
+},
+{
+  id:"item2",
+  price:30,
+  qty:2,
+  text:"Add To Cart"  
+},
+{
+  id:"item3",
+  price:10,
+  qty:3,
+  text:"Add To Cart"  
+}
+];
+
+productresult = 0 ;
+add(y){  
+
+  if( y.text == 'Add To Cart')
+  {
+this.productresult = this.productresult + y.price * y.qty;
+y.text="Remove Cart";
+  }
+  else
+  {
+    this.productresult = this.productresult - y.price * y.qty;
+y.text="Add To Cart";
+
+  }
+
+}
+
   ngOnInit() {
   }
 
