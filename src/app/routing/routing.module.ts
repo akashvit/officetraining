0e3dfbb0cabd84e9c55023a2ad94bb26a21fcad3
Akash Thengre <AThengre@vidushigoc.local>
2018-07-12 15:11:48 +0530

    store

---
+++ src/app/routing/routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from '../home/home.component';
+import {RouterModule , Routes} from '@angular/router';
+
+const routes : Routes = [ 
+  {path:'home',component:HomeComponent},
+  {path:'',component:HomeComponent}  
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports : [RouterModule],
+  declarations: []
+})
+export class RoutingModule { }
